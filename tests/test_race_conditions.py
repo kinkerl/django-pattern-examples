079fb70da0baf380653eecb2c3693ba63ebe4eb7
Dennis Schwertel <s@digitalkultur.net>
2026-02-22 16:40:18 +0100

    enhancement: added docstrings, made nicer

--- tests/test_race_conditions.py
+++ tests/test_race_conditions.py
@@ -8,86 +8,135 @@ from django.db.models import F
 from app.models import Account
 
 
-def deposit_naive(account_id, amount=100, delay=0):
+def deposit_naive(account_id, amount=100, delay=1):
+    """
+    This will reliably trigger a race condition on the update of the balance
+    attribute when called multiple times simultaneously.
+    """
     acc = Account.objects.get(id=account_id)
 
-    if delay:
-        time.sleep(delay)
+    time.sleep(delay)
 
     acc.balance = acc.balance + amount
     acc.save()
 
 
-def deposit_select_related(account_id, amount=100, delay=0):
+def deposit_select_related(account_id, amount=100, delay=1):
+    """
+    This examples shows that select_related does not help you ;)
+    """
+
     acc = Account.objects.select_related().get(id=account_id)
 
-    if delay:
-        time.sleep(delay)
+    time.sleep(delay)
 
     acc.balance = acc.balance + amount
     acc.save()
 
 
-def deposit_select_for_update_slow(account_id, amount=100, delay=0):
+def deposit_select_for_update_slow(account_id, amount=100, delay=1):
+    """
+    This function acquires an exclusive lock on the account row in a
+    transaction, ensuring that no other transaction can read or modify it
+    until the current transaction is complete.
+
+    Because of this, slow operations - like the sleep in this example - makes
+    it very slow with high concurrency.
+    Always try to keep these kind of transactions as fast as possible.
+
+    See deposit_select_for_update_fast
+    """
+
     with transaction.atomic():
         acc = Account.objects.select_for_update().get(id=account_id)
 
-        if delay:
-            # TODO: Add note why long running tasks are bad in a transaction
-            time.sleep(delay)
-
-        acc.balance = acc.balance + amount
-        acc.save()
-
-
-def deposit_select_for_update_fast(account_id, amount=100, delay=0):
-    if delay:
         time.sleep(delay)
 
+        acc.balance = acc.balance + amount
+        acc.save()
+
+
+def deposit_select_for_update_fast(account_id, amount=100, delay=1):
+    """
+    Same as deposit_select_for_update_slow but the slow operation is not part
+    of the transaction. This improves overall execution time dramatically.
+
+    See deposit_select_for_update_slow
+    """
+
+    time.sleep(delay)
+
     with transaction.atomic():
         acc = Account.objects.select_for_update().get(id=account_id)
         acc.balance = acc.balance + amount
         acc.save()
 
 
-def deposit_atomic(account_id, amount=100, delay=0):
+def deposit_atomic(account_id, amount=100, delay=1):
+    """
+    The update (balance = balance + amount) happens entirely in the database,
+    not in Python. With F(), the database handles the math, ensuring atomicit
+    even with high concurrency.
+
+    In the real world, you often can not use this shortcut to guard against
+    race conditions and must rely on other methods like select_for_update.
+
+    See deposit_select_for_update_fast
+    """
+    time.sleep(delay)
     Account.objects.filter(id=account_id).update(balance=F("balance") + amount)
 
 
 @pytest.mark.django_db(transaction=True)
 @pytest.mark.parametrize(
-    ("f", "fail_reason"),
+    ("f", "min_time", "max_time", "fail_reason"),
     [
         (
             deposit_naive,
+            1,
+            2,
             "Race condition during parallel execution found because nothing was done to prevent it.",
         ),
         (
             deposit_select_related,
+            1,
+            2,
             "Race condition during parallel execution found because select_related does not protect against it.",
         ),
-        (deposit_select_for_update_slow, ""),
-        (deposit_select_for_update_fast, ""),
-        (deposit_atomic, ""),
+        (deposit_select_for_update_slow, 10, 11, ""),
+        (deposit_select_for_update_fast, 1, 2, ""),
+        (deposit_atomic, 1, 2, ""),
     ],
 )
-def test_raceconditions(f, fail_reason):
-    acc = Account.objects.create(owner="carol", balance=0)
-
+def test_raceconditions(f, min_time, max_time, fail_reason):
+    # Test setup
     number_of_threads = 10
     balance_increment = 100
 
+    acc = Account.objects.create(owner="Nora", balance=0)
+
+    # Prepare and start all the threads
     threads = [
         threading.Thread(target=f, args=(acc.id, balance_increment, 1))
         for _ in range(number_of_threads)
     ]
 
+    time_start = time.perf_counter()
     for t in threads:
         t.start()
 
     for t in threads:
         t.join()
+    time_end = time.perf_counter()
 
+    # Making sure that the execution time is in the limits we assume
+    # This makes sure we actually run into race conditions
+    elapsed = time_end - time_start
+
+    assert elapsed > min_time
+    assert elapsed < max_time
+
+    # Making sure to get the latest version from the database for the assert
     acc.refresh_from_db()
 
     if fail_reason:
